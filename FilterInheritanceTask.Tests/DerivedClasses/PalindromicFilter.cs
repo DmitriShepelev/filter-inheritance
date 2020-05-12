17c877f2f1d4198eb821e29970948e3439afef8a
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-12 14:59:19 +0000

    add-palindromic-filter-file

---
+++ FilterInheritanceTask.Tests/DerivedClasses/PalindromicFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FilterInheritanceTask.Tests.DerivedClasses
+{
+    public class PalindromicFilter : Filter
+    {
+        //TODO: Add code here if necessary or delete this comment.
+
+        protected override bool Verify(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
