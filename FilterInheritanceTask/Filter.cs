3f753b6a51769e93bf97b1776a63cefc6c8ebccd
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-12 14:57:02 +0000

    add-task-file

---
+++ FilterInheritanceTask/Filter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FilterInheritanceTask
+{
+    public abstract class Filter
+    {
+        public int[] Select(int[] source)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract bool Verify(int number);
+    }
+}
