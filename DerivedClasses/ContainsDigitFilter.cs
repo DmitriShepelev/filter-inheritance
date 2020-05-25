c6aad2bd72e051b8d3c0782fd2635efbcb73099d
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-25 15:05:17 +0000

    add-first-derived-class

---
+++ DerivedClasses/ContainsDigitFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FilterInheritanceTask;
+
+namespace DerivedClasses
+{
+    public class ContainsDigitFilter : Filter
+    {
+
+        //TODO: Add code here if necessary or delete this comment.
+        public int Digit
+        {
+            get => throw new NotImplementedException();
+            set=> throw new NotImplementedException();
+        }
+
+        protected override bool Verify(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
