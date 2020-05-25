15356e17e3f5e7fcb403e28dd35f0cc90487d77f
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-25 15:05:45 +0000

    add-second-derived-class

---
+++ DerivedClasses/PalindromicFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FilterInheritanceTask;
+
+namespace DerivedClasses
+{
+    public class PalindromicFilter : Filter
+    {
+        //TODO: Add code here if necessary or delete this comment.
+
+        protected override bool Verify(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
