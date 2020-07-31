d26cfe687d238e41bdbfdb124f813e64a675a8ae
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 13:20:41 +0000

    Replace Filter.cs

--- FilterInheritanceTask/Filter.cs
+++ FilterInheritanceTask/Filter.cs
@@ -2,13 +2,29 @@
 
 namespace FilterInheritanceTask
 {
+    /// <summary>
+    ///  Represents the base class for filter classes by some predicate.
+    /// </summary>
     public abstract class Filter
     {
+        /// <summary>
+        /// Filters an array of integers based on a predicate.
+        /// </summary>
+        /// <param name="source"> An array of integers. </param>
+        /// <returns>An array that contains elements from the input array that satisfy the condition.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when array is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when array is empty.</exception>
         public int[] Select(int[] source)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Verifies if a given number satisfies a given condition. 
+        /// </summary>
+        /// <remarks>Must be implemented in derived classes.</remarks>
+        /// <param name="number">A given number.</param>
+        /// <returns>true if number satisfy the condition; false otherwise.</returns>
         protected abstract bool Verify(int number);
     }
 }
