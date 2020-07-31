c8fb236863877dcef2ee06ce94218bbcdb1f1438
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 13:20:17 +0000

    Replace PalindromicFilter.cs

--- DerivedClasses/PalindromicFilter.cs
+++ DerivedClasses/PalindromicFilter.cs
@@ -5,8 +5,6 @@ namespace DerivedClasses
 {
     public class PalindromicFilter : Filter
     {
-        //TODO: Add code here if necessary or delete this comment.
-
         protected override bool Verify(int number)
         {
             throw new NotImplementedException();
