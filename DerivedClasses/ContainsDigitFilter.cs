733272365e06931b8607c183c97d8bdd067329c3
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 13:19:59 +0000

    Replace ContainsDigitFilter.cs

--- DerivedClasses/ContainsDigitFilter.cs
+++ DerivedClasses/ContainsDigitFilter.cs
@@ -5,12 +5,10 @@ namespace DerivedClasses
 {
     public class ContainsDigitFilter : Filter
     {
-
-        //TODO: Add code here if necessary or delete this comment.
         public int Digit
         {
             get => throw new NotImplementedException();
-            set=> throw new NotImplementedException();
+            set => throw new NotImplementedException();
         }
 
         protected override bool Verify(int number)
